b43784d7f7355ed858ccf4758d6c2c9e163915a2
Koihik <koihik@hotmail.com>
2018-11-02 18:56:30 +0800

    INIT

---
+++ README.md
@@ -0,0 +1,13 @@
+# vscode-lua-format
+
+VS Code plugin for [LuaFormatter](https://github.com/Koihik/LuaFormatter).
+
+## Features
+Reformats your Lua source code.
+
+## Requirements
+Only Support MacOS/Linux now.
+
+## Extension Settings
+
+* `vscode-lua-foramt.configFile`: Specify the style config file
---
+++ src/extension.ts
@@ -0,0 +1,73 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import * as os from 'os';
+
+const selectors: { language: string; scheme: string }[] = [
+    { language: 'lua', scheme: 'file' },
+    { language: 'lua', scheme: 'untitled' },
+];
+
+let extensionPath = "";
+
+export function activate(context: vscode.ExtensionContext) {
+    extensionPath = context.extensionPath;
+
+    vscode.languages.registerDocumentFormattingEditProvider(selectors, new LuaFormatProvider());
+}
+
+export function deactivate() {
+}
+
+class LuaFormatProvider implements vscode.DocumentFormattingEditProvider {
+
+    constructor() {
+    }
+
+    public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+        var data = document.getText();
+
+        return new Promise((resolve, reject) => {
+
+            let configPath = vscode.workspace.getConfiguration().get<string>("vscode-lua-format.configPath");
+
+            const args = ["-si"];
+
+            if (configPath) {
+                args.push("-c");
+                args.push(configPath);
+            }
+
+            let platform = os.platform();
+            var path = `${extensionPath}/bin/`;
+            if (platform == "linux" || platform == "darwin") {
+                path += platform;
+            } else {
+                vscode.window.showErrorMessage(`vscode-lua-format do not support Windows.`);
+                reject(new Error(`vscode-lua-format do not support Windows.`));
+            }
+            path += "/lua-format";
+
+            const cmd = cp.spawn(path, args, {});
+            let result = "";
+            cmd.on('error', err => {
+                console.warn(err);
+                vscode.window.showErrorMessage(`Run lua-format error : '${err.message}'`);
+                reject(err);
+            });
+            cmd.stdout.on('data', data => {
+                result += data.toString();
+            });
+            cmd.on('exit', code => {
+                if (code) {
+                    vscode.window.showErrorMessage(`Run lua-format failed with exit code: ${code}`);
+                    return reject(new Error(`Run lua-format failed with exit code: ${code}`));
+                }
+                const range = document.validateRange(new vscode.Range(0, 0, Infinity, Infinity));
+                resolve([new vscode.TextEdit(range, result)]);
+            });
+            cmd.stdin.write(data);
+            cmd.stdin.end();
+        });
+    }
+}
